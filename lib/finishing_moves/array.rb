ff4cb8f21f0ee823315fc24dd48cadb0143b7922
SkyBirdSoar <spfdisposable-github@yahoo.com.sg>
2015-01-15 00:52:18 +0800

    Yay new array methods! Great idea!

--- lib/finishing_moves/array.rb
+++ lib/finishing_moves/array.rb
@@ -1,16 +1,38 @@
 class Array
-
-  def fill_hash(starting_key = 0)
+  def to_hash_as_values(starting_key = 0, &block)
     key = starting_key
-    Hash[self.map do |value|
-      add = [key, value]
-      key += 1
-      next add
-    end]
+    block = proc { |key| key + 1 } unless block_given?
+    t = {}
+    each do |entry|
+      t[key] = entry
+      key = block.call key
+    end
+    t
+  end
+  alias_method :use_as_values_for_hash, :to_hash_as_values 
+  alias_method :use_as_values, :to_hash_as_values
+
+  # original fill_hash method (which was inefficient there is at least 2 iterations. (Does hash also check if each new key exists? If so then 3...))
+  # Did not agree with the name
+  # also map is a instance method
+  # do not need to prefix self when using map
+  def to_indexed_hash(starting_key = 0)
+    raise ArgumentError, '#{starting_key.inspect} is not an integer' unless starting_key.is_a? Integer
+    to_hash_as_values(starting_key) { |i| i + 1 }
   end
 
-  def fill_hash_keys(default_value = 0)
-    Hash[self.map { |key| [key, default_value] } ]
+  # original fill_hash_keys method (inefficient as stated above)
+  # also did not agree with the name
+  # default value should usually be nil.
+  def to_hash_as_keys(default_value = nil, &block)
+    t = {}
+    block = proc { default_value } unless block_given?
+    each do |entry|
+      t[entry] = block.call entry
+    end
+    t
   end
+  alias_method :use_as_keys_for_hash, :to_hash_as_keys
+  alias_method :use_as_keys, :to_hash_as_keys
 
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -6,15 +6,15 @@ describe Array do
     ['Rauru', 'Saria', 'Darunia', 'Princess Ruto', 'Nabooru', 'Impa', 'Zelda']
   end
 
-  it '#fill_hash' do
-    sages_hash = sages.fill_hash
+  it '#to_indexed_hash' do
+    sages_hash = sages.to_indexed_hash
     i = 0
     sages.each do |sage|
       expect(sages_hash[i]).to eq sage
       i += 1
     end
 
-    sages_hash = sages.fill_hash(22)
+    sages_hash = sages.to_indexed_hash(22)
     i = 22
     sages.each do |sage|
       expect(sages_hash[i]).to eq sage
@@ -22,17 +22,69 @@ describe Array do
     end
   end
 
-  it '#fill_hash_keys' do
-    sages_hash = sages.fill_hash_keys
+  it '#to_hash_as_values <#to_indexed_hash behaviour>' do
+    block = proc { |key| key + 1 }
+    sages_hash = sages.to_hash_as_values 0, &block
+    # alias check
+    _ = sages.use_as_values_for_hash 0, &block
+    __ = sages.use_as_values 0, &block
+    expect((sages_hash == _) && (sages_hash == __)).to eq true
+
+    i = 0
     sages.each do |sage|
-      expect(sages_hash[sage]).not_to eq nil
-      expect(sages_hash[sage]).to eq 0
+      expect(sages_hash[i]).to eq sage
+      i += 1
     end
 
-    sages_hash = sages.fill_hash_keys('foobar')
+    sages_hash = sages.to_hash_as_values(22)
+    i = 22
     sages.each do |sage|
-      expect(sages_hash[sage]).not_to eq nil
+      expect(sages_hash[i]).to eq sage
+      i += 1
+    end
+  end
+
+  it '#to_hash_as_values' do
+    block = proc { |key| key + 3 }
+    sages_hash = sages.to_hash_as_values 0, &block
+    # alias check
+    _ = sages.use_as_values_for_hash 0, &block
+    __ = sages.use_as_values 0, &block
+    expect((sages_hash == _) && (sages_hash == __)).to eq true
+
+    i = 0
+    sages.each do |sage|
+      expect(sages_hash[i]).to eq sage
+      i += 3
+    end
+
+    sages_hash = sages.to_hash_as_values 22, &block
+    i = 22
+    sages.each do |sage|
+      expect(sages_hash[i]).to eq sage
+      i += 3
+    end
+  end
+
+  it '#to_hash_as_keys' do
+    sages_hash = sages.to_hash_as_keys
+    # alias check
+    _ = sages.use_as_keys_for_hash
+    __ = sages.use_as_keys
+    expect((sages_hash == _) && (sages_hash == __)).to eq true
+
+    sages.each do |sage|
+      expect(sages_hash[sage]).to eq nil
+    end
+
+    sages_hash = sages.to_hash_as_keys('foobar')
+    sages.each do |sage|
       expect(sages_hash[sage]).to eq 'foobar'
+    end
+
+    sages_hash = sages.to_hash_as_keys { |key| key + "1" }
+    sages.each do |sage|
+      expect(sages_hash[sage]).to eq sage + "1"
     end
   end
 
