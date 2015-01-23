373f3480b6d6ea55aad1595d468c444892481ccb
Frank Koehl <frank@forgecrafted.com>
2015-01-23 14:48:45 -0500

    Spec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,6 +48,11 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.order = :random
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
