3360166c08c40655edaf2fcebdccb290100cc55d
Frank Koehl <frank@forgecrafted.com>
2015-04-13 15:33:19 -0400

    Ducking ftw

--- lib/finishing_moves/date_and_time.rb
+++ lib/finishing_moves/date_and_time.rb
@@ -1,16 +1,8 @@
 module FinishingMovesFiscalLogic
 
-  attr_reader :fiscal_start
-  attr_reader :quarters
-
-  def self.included(base) # built-in Ruby hook for modules
-    base.class_eval do
-      original_method = instance_method(:initialize)
-      define_method(:initialize) do |*args, &block|
-        original_method.bind(self).call(*args, &block)
-        @fiscal_start = 1
-      end
-    end
+  def fiscal_start
+    @fiscal_start = 1 if @fiscal_start.nil?
+    @fiscal_start
   end
 
   def fiscal_start=(month)
@@ -18,7 +10,7 @@ module FinishingMovesFiscalLogic
   end
 
   def fiscal_quarter
-    (((self.month + (12 - @fiscal_start) ) / 3) % 4) + 1
+    (((self.month + (12 - fiscal_start) ) / 3) % 4) + 1
   end
 
   # return array of month integers correspond to appropriate quarter interger
