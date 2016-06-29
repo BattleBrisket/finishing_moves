81c2e542346635600202c038cf95ff33490c8d7b
Frank Koehl <fkoehl@gormanhealthgroup.com>
2016-06-29 15:09:47 -0400

    Moved methods

---
+++ lib/finishing_moves/symbol.rb
@@ -0,0 +1,17 @@
+class Symbol
+
+  # TODO
+  # Credit: http://thingsinabucket.com/2015/07/01/three_little_hacks/
+  def ~@
+    -> (o) { o.respond_to?(self) }
+  end
+
+  def keyify
+    to_s.keyify
+  end
+
+  def slugify
+    to_s.slugify
+  end
+
+end
