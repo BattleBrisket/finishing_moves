b2fd9e729c67d5303b2b5bb74d4170dfb767ba59
Frank Koehl <fkoehl@gmail.com>
2020-08-16 20:05:31 -0400

    .sample method, wip

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -27,4 +27,18 @@ describe Hash do
     expect(test_hash.delete_each!(:baz)).to eq({ })
   end
 
+  it '#sample' do
+    test_hash = {
+      "Eastern Time (US & Canada)"  => "America/New_York",
+      "Central Time (US & Canada)"  => "America/Chicago",
+      "Mountain Time (US & Canada)" => "America/Denver",
+      "Pacific Time (US & Canada)"  => "America/Los_Angeles",
+      "Indiana (East)"              => "America/Indiana/Indianapolis",
+      "Arizona"                     => "America/Phoenix",
+    }
+    test_hash_clone = test_hash.dup
+    test_hash.sample
+
+  end
+
 end
