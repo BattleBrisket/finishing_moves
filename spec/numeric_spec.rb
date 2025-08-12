b7940a1174b6792c497d77970dc1da734edeca9c
Frank Koehl <fkoehl@gmail.com>
2025-08-12 15:16:30 -0400

    Remove alias from testing, push rev number

--- spec/numeric_spec.rb
+++ spec/numeric_spec.rb
@@ -10,12 +10,8 @@ describe "Numeric methods" do
     expect(900.length).to eq 3
     expect(9000.length).to eq 4
     expect(-9000.length).to eq 4
-    # alias
-    expect(-9000.digits).to eq 4
     expect(12356469787881584554556.length).to eq 23
     expect(-12356469787881584554556.length).to eq 23
-    # alias
-    expect(-12356469787881584554556.digits).to eq 23
   end
 
   it "Float#length" do
@@ -24,8 +20,6 @@ describe "Numeric methods" do
     expect{ -1.0.length }.to raise_error(ArgumentError)
     expect{ 3.14.length }.to raise_error(ArgumentError)
     expect{ 12356469.987.length }.to raise_error(ArgumentError)
-    # alias
-    expect{ 12356469.987.digits }.to raise_error(ArgumentError)
   end
 
   it "BigDecimal#length" do
@@ -33,8 +27,6 @@ describe "Numeric methods" do
     expect{ -1265437718438866624512.123.length }.to raise_error(ArgumentError)
     expect{ 0.9999999999999062.length }.to raise_error(ArgumentError)
     expect{ -0.9999999999999062.length }.to raise_error(ArgumentError)
-    # alias
-    expect{ -0.9999999999999062.digits }.to raise_error(ArgumentError)
   end
 
   it "Integer#subtract_percent" do
