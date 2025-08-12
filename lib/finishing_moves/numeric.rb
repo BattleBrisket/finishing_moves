0fa1573a6236a2f8dd06547e471888b352e16613
Frank Koehl <fkoehl@gmail.com>
2025-08-12 15:10:28 -0400

    Remove digits alias from Integer (now a method in core Ruby)

--- lib/finishing_moves/numeric.rb
+++ lib/finishing_moves/numeric.rb
@@ -2,7 +2,6 @@ class Numeric
   def length
     raise ArgumentError.new("Cannot get length: \"#{self}\" is not an integer")
   end
-  alias_method :digits, :length
 
   def subtract_percent(percent)
     self.to_f * ( ( 100.0 - percent.to_f ) / 100.0 )
@@ -19,5 +18,4 @@ class Integer
   def length
     Math.log10(self.abs).to_i + 1
   end
-  alias_method :digits, :length
 end
